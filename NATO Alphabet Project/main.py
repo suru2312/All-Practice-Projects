c06a59e9ace9e47edd941ea804ad5b08b3e33409
Suraj Sharma <surajsharma23122001@gmail.com>
2026-04-10 00:59:44 +0530

    NATO Project Modification

--- NATO Alphabet Project/main.py
+++ NATO Alphabet Project/main.py
@@ -1,9 +1,14 @@
 import pandas as pd
-df = pd.read_csv(r"D:\Python Bootcamp\All Practice Projects\NATO Alphabet Project\nato_phonetic_alphabet.csv")
 
-nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
-
-user_input = input("Enter your first name : ").upper()
-user_name_list = [i for i in user_input]
-nato_list = [nato_dict[i] for i in user_name_list]
-print(nato_list)
+is_on = True
+while is_on:
+    try:
+        df = pd.read_csv(r"D:\Python Bootcamp\All Practice Projects\NATO Alphabet Project\nato_phonetic_alphabet.csv")
+        nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+        user_input = input("Enter your first name : ").upper()
+        user_name_list = [i for i in user_input]
+        nato_list = [nato_dict[i] for i in user_name_list]
+        print(nato_list)
+        is_on = False
+    except:
+        print("Only alphabets in the name.\n")
